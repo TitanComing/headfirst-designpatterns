55e4e2462909358a830158dc1408eaf9f984bf24
peng <1740298094@qq.com>
2020-06-30 23:15:04 +0800

    增加了状态模式

---
+++ src/headfirst/learndesignpatterns/composite/menu/Waitress.java
@@ -0,0 +1,13 @@
+package headfirst.learndesignpatterns.composite.menu;
+
+public class Waitress {
+	MenuComponent allMenus;
+ 
+	public Waitress(MenuComponent allMenus) {
+		this.allMenus = allMenus;
+	}
+ 
+	public void printMenu() {
+		allMenus.print();
+	}
+}
